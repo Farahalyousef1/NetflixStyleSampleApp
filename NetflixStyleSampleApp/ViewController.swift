29d8af720e8ea77a3157ba74c72f3c60bdfaff07
Minji Kim <minji_0801@naver.com>
2021-10-12 18:46:59 +0900

    Initial Commit

---
+++ NetflixStyleSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixStyleSampleApp
+//
+//  Created by 김민지 on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
