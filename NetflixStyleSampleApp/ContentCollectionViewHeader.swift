872e2e63ccb85c1d41ef992e7c9083e7fc8cc101
Minji Kim <minji_0801@naver.com>
2021-10-14 13:11:33 +0900

    넷플릭스 스타일 영화 추천 앱 구현 (Collection View Study)
    * 스토리보드 사용하지 않고 코드로 UI 구성
    * Collection View Section, Layout

---
+++ NetflixStyleSampleApp/ContentCollectionViewHeader.swift
@@ -0,0 +1,25 @@
+//
+//  ContentCollectionViewHeader.swift
+//  NetflixStyleSampleApp
+//
+//  Created by 김민지 on 2021/10/13.
+//
+
+import UIKit
+
+class ContentCollectionViewHeader: UICollectionReusableView {
+    let sectionNameLabel = UILabel()
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        sectionNameLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        sectionNameLabel.textColor = .white
+        sectionNameLabel.sizeToFit()
+        
+        addSubview(sectionNameLabel)
+        sectionNameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.top.bottom.leading.equalToSuperview().offset(10)
+        }
+    }
+}
